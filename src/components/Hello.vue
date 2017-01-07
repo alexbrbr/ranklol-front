3a89e408c4d6e61ec03e2f330207bb5fbe463251
Alexandre Barbier <a.barbier@epmistes.net>
2017-01-07 12:59:27 +0100

    feat(matches): use computed properties to update data

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -9,6 +9,12 @@
      v-on:click="loadSummonerData">
      Load summoner data
     </button>
+    <div>
+      Most played role : {{numberOfMostPlayedRole}} matches as {{mostPlayedRole}}
+    </div>
+    <div>
+      Most played day : {{numberOfMostPlayedDay}} matches on {{mostPlayedDay}}
+    </div>
     <div v-if="!results">
       Loading
     </div>
@@ -40,8 +46,34 @@ export default {
   },
   data () {
     return {
-      results: null,
+      results: {},
       summonerName: 'UOL Vizicsacsi'
+    }
+  },
+  computed: {
+    mostPlayedRole: function () {
+      return this.results.rolesData && Object
+        .keys(this.results.rolesData)
+        .reduce((mostPlayed, curr) => {
+          return this.results.rolesData[curr] > this.results.rolesData[mostPlayed]
+            ? curr
+            : mostPlayed
+        })
+    },
+    numberOfMostPlayedRole: function () {
+      return this.results.rolesData && this.results.rolesData[this.mostPlayedRole]
+    },
+    mostPlayedDay: function () {
+      return this.results.daysData && Object
+        .keys(this.results.daysData)
+        .reduce((mostPlayed, curr) => {
+          return this.results.daysData[curr] > this.results.daysData[mostPlayed]
+          ? curr
+          : mostPlayed
+        })
+    },
+    numberOfMostPlayedDay: function () {
+      return this.results.daysData && this.results.daysData[this.mostPlayedDay]
     }
   }
 }
